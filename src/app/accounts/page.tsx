7f0088cddd61105c9959d392768fea146380ebb9
AadityaJujagar <aadityajujagar@gmail.com>
2023-11-23 17:07:14 +0530

    project finished

--- src/app/accounts/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Accounts() {
-  return <div className="accounts">Accounts</div>;
-}
--- src/app/favourites/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Favourites() {
-  return <div className="favourites">Favourites</div>;
-}
